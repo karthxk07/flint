93f781122942608dc90d5891bd7a429e2a54c5ac
Karthik <karthikkalyanam07@gmail.com>
2023-09-17 14:54:41 +0530

    Error fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { useAuthContext } from "@/src/contexts/AuthContext";
 import { useRouter } from "next/navigation";
-// import LoginPageView from "@/views/LoginPageView";
+import React, { useEffect } from "react";
 
 export default function Home() {
   const { authState, setAuthState } = useAuthContext() as any;
 
-  const router = useRouter() as any;
-
-  return <>{authState == null ? <>{router.push("/login")}</> : <p>Home</p>}</>;
+  const router = useRouter();
+  useEffect(() => {
+    authState == null && router.push("/login");
+  }, []);
 }
