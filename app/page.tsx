ef1d1f598dcb2563309309474a81b039ff81c6da
Karthik <109301536+karthxk07@users.noreply.github.com>
2023-09-17 18:44:26 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    authState.isAuthenticated === true && router.push("/login");
+    authState.isAuthenticated === false && router.push("/login");
   }, []);
 
   return (
