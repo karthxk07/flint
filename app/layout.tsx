b4bead0caefad19434af54ce1dd8b5d725dcf274
Karthik <109301536+karthxk07@users.noreply.github.com>
2023-09-18 19:45:28 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="font-VarelaRound">
+      <body className="font-[Helvetica]">
         <AuthContextProvider>{children}</AuthContextProvider>
       </body>
     </html>
