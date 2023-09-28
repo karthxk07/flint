9c507517dfcf05321b588eab696134c311fe4ad3
Karthik <karthikkalyanam07@gmail.com>
2023-09-28 23:39:20 +0530

    Added route groups

---
+++ src/app/(user)/layout.tsx
@@ -0,0 +1,18 @@
+import CompactSideBar from "@/src/components/Inbox/CompactSidebar";
+import Contacts from "@/src/components/Inbox/Contacts";
+
+export default function InboxLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <div className="flex flex-row h-screen overflow-clip">
+        <CompactSideBar />
+
+        {children}
+      </div>
+    </>
+  );
+}
